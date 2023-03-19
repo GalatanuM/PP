2a747ec92b7fe241dbdf879062d2a5632a17aa18
GalatanuM <marco-ionut.galatanu@student.tuiasi.ro>
2023-03-19 22:52:45 +0200

    modificat functie afisare raw, html, json

--- lab4/Ex1_Ex2/src/main/kotlin/LibraryPrinter.kt
+++ lab4/Ex1_Ex2/src/main/kotlin/LibraryPrinter.kt
@@ -6,20 +6,20 @@ open class LibraryPrinter
         {
             println("Autor: " + it.getAuthor())
             println("Text: " + it.getText())
-            println("Name: " + it.getName())
-            println("Publisher: " + it.getPublisher())
-            println("Price: " + it.getPrice())
+            println("Nume: " + it.getName())
+            println("Publicatie: " + it.getPublisher())
+            println("Pret: " + it.getPrice())
             println()
         }
     }
     fun printHTML(books: Set<Book>) {
         println("<html>")
-        println("\t<head><title>Books</title></head>")
+        println("\t<head><title>Carti</title></head>")
         println("\t<body>")
-        println("\t\t<h1>Books</h1>")
+        println("\t\t<h1>Carti</h1>")
         println("\t\t<ul>")
         books.forEach {
-            println("\t\t\t<li>${it.getName()} by ${it.getAuthor()}, published by ${it.getPublisher()} worth ${it.getPrice()}</li>")
+            println("\t\t\t<li>${it.getName()} scrisa de ${it.getAuthor()}, publicata de ${it.getPublisher()} care costa ${it.getPrice()}</li>")
         }
         println("\t\t</ul>")
         println("\t</body>")
@@ -32,10 +32,10 @@ open class LibraryPrinter
             jsonBooks.add(
                 """
                 {
-                    "name": "${it.getName()}",
-                    "author": "${it.getAuthor()}",
-                    "publisher": "${it.getPublisher()}",
-                    "price": ${it.getPrice()}
+                    "Nume": "${it.getName()}",
+                    "Autor": "${it.getAuthor()}",
+                    "Publicatie": "${it.getPublisher()}",
+                    "Pret": ${it.getPrice()}
                 }
                 """.trimIndent()
                          )
--- lab4/Ex1_Ex2/src/main/kotlin/Main.kt
+++ lab4/Ex1_Ex2/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val content1 = Content("Author 1", "Text 1", "Book 1", "Publisher 1")
-    val content2 = Content("Author 2", "Text 2", "Book 2", "Publisher 1")
-    val content3 = Content("Author 1", "Text 3", "Book 3", "Publisher 2")
+    val content1 = Content("Marco", "haha", "Carte 1", "Publicatia 1")
+    val content2 = Content("Alin", "hehe", "Carte 2", "Publicatia 2")
+    val content3 = Content("Ana", "hihi", "Carte 2", "Publicatia 1")
 
     val book1 = Book(content1, 10.99)
     val book2 = Book(content2, 12.99)
@@ -12,32 +12,32 @@ fun main() {
     library.addBook(book2)
     library.addBook(book3)
 
-    val booksByAuthor = library.findAllByAuthor("Author 1")
-    println("Books by Author 1:")
+    val booksByAuthor = library.findAllByAuthor("Marco")
+    println("Cartile lui Marco:")
     booksByAuthor.forEach { println(it.getName()) }
     println()
 
-    val booksByName = library.findAllByName("Book 2")
-    println("Books named Book 2 by authors:")
+    val booksByName = library.findAllByName("Carte 2")
+    println("Cartile numite Carte 2 scrise de:")
     booksByName.forEach { println(it.getAuthor()) }
     println()
 
-    val booksByPublisher = library.findAllByPublisher("Publisher 1")
-    println("Books published by Publisher 1:")
+    val booksByPublisher = library.findAllByPublisher("Publicatia 1")
+    println("Cartile publicate de Publicatia 1:")
     booksByPublisher.forEach { println(it.getName()) }
     println()
 
     val libraryprinter = LibraryPrinter()
 
-    println("Books by Author 1 RAW:\n")
+    println("Cartile lui Marco RAW:\n")
     libraryprinter.printBooksRaw(booksByAuthor)
     println()
 
-    println("Books by Author 1 in HTML:")
+    println("Cartile lui Marco in HTML:")
     libraryprinter.printHTML(booksByAuthor)
     println()
 
-    println("Books by Author 1 in JSON:")
+    println("Cartile lui Marco in JSON:")
     libraryprinter.printJSON(booksByAuthor)
     println()
 }
